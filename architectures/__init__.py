0d44eecffad9b41b1bc4131ae531459c0cad152c
SabrinaDu <sabrina.du1972@gmail.com>
2025-09-21 22:10:50 -0400

    Suppressed timm FutureWarning about deprecated imports. Same suppression is in ava

--- architectures/__init__.py
+++ architectures/__init__.py
@@ -1,3 +1,13 @@
+import warnings
+
 from .elastic_mae import ElasticMae, SimpleAMEGlimpseElasticMae, DivideFourGlimpseElasticMae, StamlikeGlimpseElasticMae, \
     StamlikeSaliencyGlimpseElasticMae, DivideFourSaliencyGlimpseElasticMae, HybridElasticMae
 from .rl_glimpse import ReconstructionRlMAE, ClassificationRlMAE, SegmentationRlMAE
+
+# Suppress timm FutureWarning about deprecated imports
+warnings.filterwarnings(
+    "ignore",
+    message="Importing from timm.models.layers is deprecated",
+    category=FutureWarning,
+    module="timm.*",
+)
